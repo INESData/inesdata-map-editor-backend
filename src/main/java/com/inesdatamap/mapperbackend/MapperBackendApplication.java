259cbb04e1a4c84e7032a3c64ab88ac39809c627
Guillermo Monzó <gcmonzo@gmv.com>
2024-05-28 18:29:27 +0200

    initial commit

---
+++ src/main/java/com/inesdatamap/mapperbackend/MapperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inesdatamap.mapperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperBackendApplication.class, args);
+	}
+
+}
