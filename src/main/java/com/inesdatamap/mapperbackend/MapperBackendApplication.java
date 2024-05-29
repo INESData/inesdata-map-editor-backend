5aeab39fab4ebd7f28bc7fa133f8ad7fd0311594
Guillermo Monzó <gcmonzo@gmv.com>
2024-05-29 09:43:49 +0200

    docs: javadoc

--- src/main/java/com/inesdatamap/mapperbackend/MapperBackendApplication.java
+++ src/main/java/com/inesdatamap/mapperbackend/MapperBackendApplication.java
@@ -3,9 +3,18 @@ package com.inesdatamap.mapperbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot application
+ */
 @SpringBootApplication
 public class MapperBackendApplication {
 
+	/**
+	 * Main method
+	 *
+	 * @param args
+	 * 		the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MapperBackendApplication.class, args);
 	}
