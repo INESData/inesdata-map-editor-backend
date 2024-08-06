40169a1a2368814578fa2887716e97dc17616fd0
SSRV <sandra.rodriguez.v@gmv.com>
2024-08-06 14:34:30 +0200

    Remove get/set and use lombok

--- src/main/java/com/inesdatamap/mapperbackend/model/jpa/DataSource.java
+++ src/main/java/com/inesdatamap/mapperbackend/model/jpa/DataSource.java
@@ -10,7 +10,6 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 /**
  * DataSource db entity representation
@@ -19,7 +18,6 @@ import lombok.ToString;
  */
 @Getter
 @Setter
-@ToString
 @Entity
 @Table(name = "DATA_SOURCE")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -36,20 +34,4 @@ public class DataSource extends BaseEntity implements Serializable {
 	 */
 	@Column(name = "name")
 	private String name;
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return this.name;
-	}
-
-	/**
-	 * @param name
-	 * 	the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
 }
