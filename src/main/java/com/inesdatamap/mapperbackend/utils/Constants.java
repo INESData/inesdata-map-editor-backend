e496610f6ba02b59b665229b3aa7ac2775b0d510
SSRV <sandra.rodriguez.v@gmv.com>
2024-08-20 12:36:55 +0200

    Remove unused constants

--- src/main/java/com/inesdatamap/mapperbackend/utils/Constants.java
+++ src/main/java/com/inesdatamap/mapperbackend/utils/Constants.java
@@ -13,27 +13,10 @@ public final class Constants {
 	 * Constant representing the number 0
 	 */
 	public static final int NUMBER_0 = 0;
-
 	/**
 	 * Constant representing the number 10
 	 */
 	public static final int NUMBER_10 = 10;
-
-	/**
-	 * Constant representing the number 16
-	 */
-	public static final int NUMBER_16 = 16;
-
-	// Passwords (for testing)
-	/**
-	 * Constant representing a test password
-	 */
-	public static final String TEST_PASSWORD = "password123";
-
-	/**
-	 * Constant representing an encoded test password
-	 */
-	public static final String ENCODED_PASSWORD = "encodedPassword";
 
 	private Constants() {
 		// Empty constructor to avoid initialization
