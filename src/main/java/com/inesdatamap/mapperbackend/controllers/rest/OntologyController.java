9ad065d5a2be31e966132d2adcbfb9d078f27d6e
SSRV <sandra.rodriguez.v@gmv.com>
2024-08-29 13:15:32 +0200

    Delete file and add body

--- src/main/java/com/inesdatamap/mapperbackend/controllers/rest/OntologyController.java
+++ src/main/java/com/inesdatamap/mapperbackend/controllers/rest/OntologyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -83,7 +84,7 @@ public class OntologyController {
 	@Operation(summary = "Update given ontology")
 	public ResponseEntity<OntologyDTO> updateOntology(
 			@PathVariable(name = "id") @Parameter(name = "id", description = "Ontology identifier to update", required = true) Long id,
-			@RequestPart("body") @Parameter(name = "ontology", description = "The ontology to update", required = true) OntologyDTO ontologyDto) {
+			@RequestBody @Parameter(name = "ontology", description = "The ontology to update", required = true) OntologyDTO ontologyDto) {
 		return ResponseEntity.ok(this.ontologyService.updateOntology(id, ontologyDto));
 	}
 
