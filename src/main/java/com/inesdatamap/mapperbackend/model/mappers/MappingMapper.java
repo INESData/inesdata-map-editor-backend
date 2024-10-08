06733c27371b7157edccca0f6a7d918120bfb00b
SSRV <sandra.rodriguez.v@gmv.com>
2024-10-08 16:28:42 +0200

    Fix merge in mapper: use custom instead of default

--- src/main/java/com/inesdatamap/mapperbackend/model/mappers/MappingMapper.java
+++ src/main/java/com/inesdatamap/mapperbackend/model/mappers/MappingMapper.java
@@ -1,14 +1,22 @@
 package com.inesdatamap.mapperbackend.model.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import com.inesdatamap.mapperbackend.model.dto.MappingDTO;
 import com.inesdatamap.mapperbackend.model.jpa.Mapping;
 
 /**
- * Mapper interface for converting between {@link com.inesdatamap.mapperbackend.model.jpa.Mapping} and {@link com.inesdatamap.mapperbackend.model.dto.MappingDTO}.
+ * Mapper interface for converting between {@link com.inesdatamap.mapperbackend.model.jpa.Mapping} and
+ * {@link com.inesdatamap.mapperbackend.model.dto.MappingDTO}.
  */
 @Mapper(componentModel = "spring", uses = MappingFieldMapper.class)
 public interface MappingMapper extends BaseEntityMapper<MappingDTO, Mapping> {
+
+	@Override
+	@org.mapstruct.Mapping(target = "id", ignore = true)
+	@org.mapstruct.Mapping(target = "version", ignore = true)
+	@org.mapstruct.Mapping(target = "rml", ignore = true)
+	Mapping merge(Mapping source, @MappingTarget Mapping target);
 
 }
