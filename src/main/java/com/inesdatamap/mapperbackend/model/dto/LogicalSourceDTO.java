550bc3a3283f2baff82e16e9f49145bbcf447511
SSRV <sandra.rodriguez.v@gmv.com>
2024-12-03 13:49:05 +0100

    Remove constraint from logical source

--- src/main/java/com/inesdatamap/mapperbackend/model/dto/LogicalSourceDTO.java
+++ src/main/java/com/inesdatamap/mapperbackend/model/dto/LogicalSourceDTO.java
@@ -1,6 +1,5 @@
 package com.inesdatamap.mapperbackend.model.dto;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +16,6 @@ public class LogicalSourceDTO extends BaseEntityDTO {
 
 	private String referenceFormulation;
 
-	@NotNull
 	private String iterator;
 
 	@Size(min = 1, max = 255)
